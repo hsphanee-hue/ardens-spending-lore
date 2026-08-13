039a202c350f427fc0c32718fadf1cbed3b4b051
arden <hsphanee@gmail.com>
2026-08-14 03:38:48 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,48 @@
+
+const CACHE_NAME = 'app-store-v2';
+
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+
 self.addEventListener('install', (e) => {
+  
+  self.skipWaiting(); 
+  
   e.waitUntil(
-    caches.open('app-store').then((cache) => {
-      return cache.addAll(['index.html', 'style.css', 'script.js']);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) 
+  );
+});
+
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
